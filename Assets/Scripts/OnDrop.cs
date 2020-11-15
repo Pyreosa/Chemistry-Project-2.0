c4bd2ac9102606b72b12c2ea8ce0f8127b3e986c
Momchil <momchil.poppff@gmail.com>
2020-11-15 18:34:17 +0200

    FirstStage

---
+++ Assets/Scripts/OnDrop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnDrop : MonoBehaviour, IDropHandler
+{
+
+    int mySpaceY = 10000;
+    private CanvasGroup canvasGroup;
+    
+    void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    void IDropHandler.OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null) 
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition * mySpaceY;
+        }
+    }
+
+    // Start is called before the first frame update
+}
