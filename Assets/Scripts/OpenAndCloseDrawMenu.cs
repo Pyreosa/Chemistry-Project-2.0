5471b3f891ccf8da4ab22020675720031ef2688e
Djccowcow <djccowcow@gmail.com>
2020-11-27 18:01:59 +0200

    Fixed DropDown

--- Assets/Scripts/OpenAndCloseDrawMenu.cs
+++ Assets/Scripts/OpenAndCloseDrawMenu.cs
@@ -1,25 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
 
 public partial class OpenAndCloseDrawMenu : MonoBehaviour
 {
-    Animator animator;
-    public GameObject[] DropDownElements;
-    public GameObject DropDownMenu;
-    
-    bool isActive;
-    
-    public void ElementsDropDown()
-    {
-         for (int i = 0 ; i < DropDownElements.Length; ++i)
-         {
-             animator = DropDownMenu.GetComponent<Animator>();
-             if (DropDownElements[i] != null && animator.GetCurrentAnimatorStateInfo(0).IsName("closedMenu") == false)
-                {
-                    isActive = DropDownElements[i].activeSelf;
-                    DropDownElements[i].SetActive(!isActive);
-                }
-        }
-    }
+	Animator animator;
+	public GameObject[] DropDownElements;
+	public GameObject DropDownMenu;
+
+	bool isActive;
+
+	float timeout = 0.8f;
+	bool canActivate = true;
+
+	public void ElementsDropDown()
+	{
+		if (canActivate)
+		{
+
+			canActivate = false;
+			for (int i = 0; i < DropDownElements.Length; ++i)
+			{
+				animator = DropDownMenu.GetComponent<Animator>();
+				if (DropDownElements[i] != null && animator.GetCurrentAnimatorStateInfo(0).IsName("closedMenu") == false)
+				{
+					isActive = DropDownElements[i].activeSelf;
+					DropDownElements[i].SetActive(!isActive);
+				}
+			}
+			Invoke("CooledDown", timeout);
+		}
+	}
+
+	void CooledDown()
+	{
+		canActivate = true;
+	}
 }
--- Assets/Scripts/OpenAndCloseMenu.cs
+++ Assets/Scripts/OpenAndCloseMenu.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class OpenAndCloseMenu : MonoBehaviour
 {
-    Animator animator;
-    public GameObject DropDownMenu;
-    
-    bool isOpen;
+	Animator animator;
+	public GameObject DropDownMenu;
 
-    public void MenuDropDown()
-    {
-        if(DropDownMenu != null)
-        {
-            animator = DropDownMenu.GetComponent<Animator>();
-            if(animator != null)
-            {
-                isOpen = animator.GetBool("open");
-                animator.SetBool("open", !isOpen);
-            }
-        }
-    }
+	bool isOpen;
+	bool canActivate = true;
+	float timeout = 0.8f;
+
+	public void MenuDropDown()
+	{
+		if (canActivate)
+		{
+
+			canActivate = false;
+			if (DropDownMenu != null)
+			{
+				animator = DropDownMenu.GetComponent<Animator>();
+				if (animator != null)
+				{
+					isOpen = animator.GetBool("open");
+					animator.SetBool("open", !isOpen);
+				}
+			}
+			Invoke("CooledDown", timeout);
+		}
+	}
+	void CooledDown()
+	{
+		canActivate = true;
+	}
 }
 
